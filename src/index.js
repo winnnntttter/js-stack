1d3eabf09208f2e4eef9b64552f2e32c6d95c54e
Dante Maycry <winnnntttter@outlook.com>
2017-09-10 21:22:39 +0800

    修改

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// @flow
+/* eslint-disable no-console */
+/* const color = require('color');
+const redHexa = color({r:255,g:0,b:0}).hex();
+console.log(redHexa) */
+/* const str = "ES6"; */
+/* const Dog = require("./dog"); */
+import Dog from './dog';
+
+const Toby = new Dog('Toby');
+console.log(Toby.bark());
+
